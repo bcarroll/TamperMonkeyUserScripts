0198b07ffe7145bc8fac88156d84ea69dbd94c71
zyphermonkey <zyphermonkey@gmail.com>
2018-08-16 12:58:38 -0400

    Update GovDeals.com.user.js

--- GovDeals.com.user.js
+++ GovDeals.com.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GovDeals.com
 // @namespace    https://github.com/bcarroll/TamperMonkeyUserScripts
-// @version      0.1
+// @version      0.2
 // @description  GovDeals.com enhancements
 // @author       Brett Carroll
 // @match        https://www.govdeals.com/*
